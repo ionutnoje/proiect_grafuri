e0ba21a094e614c2f5acce8925e652b53d6cfaad
noje_ <noje_i@yahoo.com>
2021-10-12 10:51:06 +0300

    commit

---
+++ furnici/src/Ant.java
@@ -0,0 +1,48 @@
+public class Ant
+{
+    protected int trailSize;       //marimea tuturor drumurilor...absolut toate drumurile dintre toate orasele
+    protected int trail[];          //marimea drumului pe care se afla actual furnicile
+    protected boolean visited[];
+
+
+
+    public Ant(int tourSize)                //functia de initializare a drumului si vectorului de orase vizitate sau nevizitate
+    {
+        this.trailSize = tourSize;          //se initializeaza marimea drumului
+        this.trail = new int[tourSize];     //cred ca se updateaza in timpul codului cu numarul de orase prin care a trecut
+        this.visited = new boolean[tourSize]; //vectorul de orase vizitate/nevizitate//pe parcursul programului orasele prin care s a trecut o sa se seteze pe true(se tine cont de orasele vizitate si nevizitate pentru viitoare parcurgeri)
+    }
+
+    protected void visitedCityJ(int currentIndex, int city)     //functie care seteaza orasul curent pe oras vizitat cu ajutorul functiei visited
+    {
+        trail[currentIndex + 1] = city;     //orasul actual
+        visited[city] = true;               //se seteaza pe true in vectorul visited
+    }
+
+    protected boolean visited(int i)
+    {
+        return visited[i];   //returneaza starea orasului in timpul rularii
+    }
+
+    protected double trailLenght(double graph[][])
+    {
+        double lenght = graph[trail[trailSize - 1]][trail[0]];
+        for(int i = 0; i < trailSize - 1; i++)
+        {
+            lenght += graph[trail[i]][trail[i + 1]];
+        }
+        return lenght;
+    }
+
+    protected void clear()        //functie care seteaza orasele pe false...pentru urmatoarea tura de furnici
+    {
+        for(int i = 0; i < trailSize; i++)   //loop prin toate orasele din toate nodurile
+        {
+            visited[i] = false;             //setarea oraselor pe false
+        }
+    }
+
+
+
+
+}
